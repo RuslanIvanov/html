2bef2af4679eb90a00c5b310ab4fe946c090c705
RuslanIvanov <rus_iv@list.ru>
2022-01-10 14:25:05 +0300

    100122_1425

--- mic_pc/copy.sh
+++ mic_pc/copy.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-echo "cop y to path: '$1'"
+echo "copy to path: '$1'"
+read -p "Yes?" ask
 
+if [ $ask == "Y" ] || [ $ask == "y" ]
+then
+
+echo "Copying..."
 
 cp -i ./ParentWindow.htm $1
 cp -i ./load_firmware_bpr.php $1
@@ -13,3 +18,10 @@ cp -i ./upload_firmware_bpr.php $1
 cp -i ./upload_libs.php $1
 cp -i ./upload_modules.php $1
 
+sudo chmod 664 $1/*.html
+sudo chmod 664 $1/*.htm
+sudo chmod 664 $1/*.php
+
+echo "End!"
+
+fi
